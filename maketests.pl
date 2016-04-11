21ddd06283d96b1dd1e4f00fa4b436adece3314d
Ryan Voots <ryan@voots.org>
2016-04-11 02:33:42 -0400

    Tests initially built.  Needs filtering.

--- maketests.pl
+++ maketests.pl
@@ -16,17 +16,23 @@ open(my $test_out, ">t/defs.json") or die "$!: defs.json";
 
 for my $fn (glob('evals/*.lst')) {
     open(my $fh, "<", $fn) or die "$!: $fn";
+    my $c = 0;
     while (my $code = <$fh>) {
         my ($c_out, $c_err);
 
         my $c_in = "perl $code";
 
         my $cmd = ['sudo', '/home/ryan/perl5/perlbrew/perls/perlbot-blead-intest/bin/perl', '/home/ryan/bots/perlbuut/lib/eval.pl'];
-
+        $c++;
         
-        print "perl $code";
-        run $cmd, \$c_in, \$c_out, \$c_err, timeout(30);
-        push $tests{$fn}->@*, {code => $code, out => $c_out, err => $c_err}; 
+        print "${fn}[$c]: $code";
+        eval {run $cmd, \$c_in, \$c_out, \$c_err, timeout(30);};
+
+        unless ($@) {
+            push $tests{$fn}->@*, {code => $code, out => $c_out, err => $c_err};
+        } else {
+            print "FAILED TO EVAL!\n";
+        }
     }
     close($fh);
 }
