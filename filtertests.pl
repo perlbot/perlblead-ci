5a3cef58049628c56b7b5744b7fc9ece934683a4
Ryan Voots <ryan@voots.org>
2016-04-11 13:09:06 -0400

    filter out some of the terrible tests

---
+++ filtertests.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+use feature 'postderef';
+
+use JSON::MaybeXS;
+use Data::Dumper;
+
+my $json = do {local $/; open(my $fh, "<", "t/defs.json"); <$fh>};
+my $data = decode_json $json;
+
+my ($dc, $lc, $ic);
+
+for my $f (keys $data->%*) {
+    $data->{$f} = [grep {!($_->{err} && $_->{out}) && (length($_->{err} . $_->{out}) < 1024) && !($_->{code} =~ /ENV|%::|%main/) } $data->{$f}->@* ];
+}
+
+open(my $fh, ">", "t/filtered.json");
+print $fh encode_json($data);
+close($fh);
