6041c597592efca3981cd444e8321e5391f52f2e
Ryan Voots <ryan@voots.org>
2016-04-13 19:34:14 -0400

    More weird cases filtered

--- filtertests.pl
+++ filtertests.pl
@@ -15,12 +15,12 @@ my ($dc, $lc, $ic);
 for my $f (keys $data->%*) {
     my $grepsub = sub {
            (length($_->{err} . $_->{out}) < 1024) # Ignore long outputs, they're unlikely to be stable (likely %INC and such)
-        && !($_->{code} =~ /(ENV|%::|%main|->now|time|\$\^V|\$\^T|\$\^O|version|rand|Time::Piece|[@%]INC|\$\$\W)/) 
+        && !($_->{code} =~ /(ENV|%::|%main|->now|time|\$\^V|\$\^T|\$\^O|\$\^X|version|rand|Time::Piece|[@%]INC|\$\$\W|\$\]|\$code|Carp)/) 
         && !($_->{err} =~ /Unrecognized character/)
         && !($_->{out} =~ /(CODE|HASH|SCALAR|REF|ARRAY)\(0/) # ignore stuff with refs in output
         && !($_->{err} =~ /Killed/
            ||$_->{out} =~ /Killed/)
-        && !($_->{code} =~ /(0\s*[+*\-]\s*)?(\[\]|{})(\s*[+*\-]\s*0)/)
+        && !($_->{code} =~ /(\s*[+*\-]\s*)?(\[\]|{})(\s*[+*\-]\s*)/)
     };
 
     my $mapsub = sub {
